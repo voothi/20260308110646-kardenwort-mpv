2eca503db903ef46ace0ca9ae4ef13fc173927ec
Denis Novikov <voothi@gmail.com>
2026-05-24 23:59:37 +0200

    20260524235933

--- scripts/kardenwort/main.lua
+++ scripts/kardenwort/main.lua
@@ -785,7 +785,7 @@ function show_osd(msg, dur)
         local text_x = left_x + pad_x
 
         local bg_rect = string.format(
-            "{\\an7}{\\pos(%d,%d)}{\\bord0}{\\shad0}{\\1c&H%s&}{\\1a&H%s&}{\\p1}m 0 0 l %d 0 l %d %d l 0 %d{\\p0}",
+            "{\\an7}{\\pos(%d,%d)}{\\bord0}{\\shad0}{\\3a&HFF&}{\\4a&HFF&}{\\1c&H%s&}{\\1a&H%s&}{\\p1}m 0 0 l %d 0 l %d %d l 0 %d{\\p0}",
             left_x, top_y, Options.seek_bg_color, Options.seek_bg_opacity, box_w, box_w, box_h, box_h
         )
         local text_event = string.format(
@@ -844,7 +844,7 @@ function show_seek_osd(msg, alignment)
         end
 
         local bg_rect = string.format(
-            "{\\an7}{\\pos(%d,%d)}{\\bord0}{\\shad0}{\\1c&H%s&}{\\1a&H%s&}{\\p1}m 0 0 l %d 0 l %d %d l 0 %d{\\p0}",
+            "{\\an7}{\\pos(%d,%d)}{\\bord0}{\\shad0}{\\3a&HFF&}{\\4a&HFF&}{\\1c&H%s&}{\\1a&H%s&}{\\p1}m 0 0 l %d 0 l %d %d l 0 %d{\\p0}",
             left_x, top_y, Options.seek_bg_color, Options.seek_bg_opacity, box_w, box_w, box_h, box_h
         )
         local text_event = string.format(
